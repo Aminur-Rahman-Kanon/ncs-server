bae69c62391094f2dfd1278faec86111f2932c7d
Aminur-Rahman-Kanon <kanon754@gmail.com>
2022-11-28 01:46:37 +0000

    server deploy3

--- passport.js
+++ passport.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: '274110835292-j5b2jieqd794efns4clilbsqa9u51n7g.apps.googleusercontent.com',
     clientSecret: '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',
-    callbackURL: "http://nihonchukosha.onrender.com/google/callback"
+    callbackURL: "https://nihonchukosha.onrender.com/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     
